2c20c251664622a4f56de16cad828123253e0a14
TerraBoii <kostyaborisenko05@gmail.com>
2022-06-03 19:21:21 +0300

    Added C# script

---
+++ csharp_script.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace FirstScript {
+    class Script {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to console, here you can see how magic happens. :3");
+        }
+    }
+}
