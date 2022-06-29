95030b22fa8d6ec4c2788ff459ad1d17ebf94124
TerraBoii <kostyaborisenko05@gmail.com>
2022-06-29 15:33:42 +0300

    Updated script

--- cpp_script.cpp
+++ cpp_script.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int main() {
+    int number;
+    cout << "Enter any number you like: ";
+    cin >> number;
+    cout << "I am your number ";
+    cout << number;
+    cout << "." << endl;
     cout << "Display this message." << endl;
 }
+
+// I think i need to learn some basic cpp
