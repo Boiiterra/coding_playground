a940248876b8398440281ad9b1cf84409c59133b
TerraBoii <kostyaborisenko05@gmail.com>
2022-07-02 20:35:21 +0300

    figured out

--- cpp_script.cpp
+++ cpp_script.cpp
@@ -6,10 +6,7 @@ int main() {
     int number;
     cout << "Enter any number you like: ";
     cin >> number;
-    cout << "I am your number ";
-    cout << number;
-    cout << "." << endl;
+    // printf("I am your number ", number, ".");
+    cout << "I am your number " << number << "." << endl;
     cout << "Display this message." << endl;
 }
-
-// I think i need to learn some basic cpp
