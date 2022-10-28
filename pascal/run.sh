7dbce5dcc0124241c0770fef6cb9dc49d0f8d395
TerraBoii <kostyaborisenko05@gmail.com>
2022-10-28 19:21:27 +0300

    Created run.sh script for pascal compilation

---
+++ pascal/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file=$1
+output=${file%.pas*}
+
+if [ -f ./pascal/$file ]; then
+    # echo "File $file exists and will be converted to $output.jar and executed"
+    fpc ./pascal/$file
+    mv ./pascal/$output ./pascal/out/
+    mv ./pascal/$output.o ./pascal/out/
+    ./pascal/out/$output
+else
+    echo "File \"$file\" does not exist"
+    echo "Or try to run this file in the scope of coding_playground directory"
+fi
