9420185eb42a1fd6844abe5950fd3a887aa42f68
TerraBoii <kostyaborisenko05@gmail.com>
2022-10-28 19:04:48 +0300

    Variables

---
+++ kotlin/vars.kt
@@ -0,0 +1,20 @@
+package vars
+
+fun main() {
+    // Constants values
+    val str: String = "I am constant"
+    val wow = "No need to type type"
+
+    // Variables
+    var integer: Int = 23
+    var hello = "Bonjour"
+
+    // Output options
+    println("Constant values:")
+    println("str -> $str")
+    println("wow -> $wow")
+    println("Variables:")
+    println("integer -> $integer")
+    println("hello -> $hello")
+    println("integer times 7 is ${integer * 7}")
+}
