f2d1774c02935087a8f74b85e67d3544a6c6c25b
TerraBoii <kostyaborisenko05@gmail.com>
2022-11-01 20:17:08 +0300

    java

---
+++ java_lang/Main.java
@@ -0,0 +1,8 @@
+package java_lang;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is new lang. :P");
+        System.out.println("This is just good!");
+    }
+}
