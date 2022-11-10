ef08ec047dd998733a0100cdabbdf9fcee4d5924
TerraBoii <kostyaborisenko05@gmail.com>
2022-11-10 18:33:01 +0300

    More nulls and game

--- kotlin/null.kt
+++ kotlin/null.kt
@@ -22,4 +22,18 @@ fun main() {
     printProduct("6", "7")
     printProduct("a", "7")
     printProduct("a", "b")
+
+    var a: String? = "old"
+    println("a is '$a'")
+    a = null
+    println("a is '$a' now")
+    // Elvis operator
+    val length = a?.length ?: -1
+    println("a's length is $length")
+
+    // !! -> throws NPE
+    println("Now we are going to see how lucky you are.")
+    val game: String? = if ((1..10).random() > 5) "Good job!!" else null
+    var luck = game!!.length
+    println("You got lucky. Luck code: $luck")
 }
