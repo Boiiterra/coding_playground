8a12c3824bc487f3698d9af114e84ff5d2794b50
TerraBoii <kostyaborisenko05@gmail.com>
2022-11-21 09:20:38 +0300

    fun

---
+++ cpp/overflow.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 2147483647;
+    cout << a << " + " << 1 << " = " << a + 1 << endl;
+    return 0;
+}
