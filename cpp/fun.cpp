6ca099d39368e709d0f5e75ddd8ce9f61e972147
TerraBoii <kostyaborisenko05@gmail.com>
2022-11-22 19:37:41 +0300

    lol what

---
+++ cpp/fun.cpp
@@ -0,0 +1,19 @@
+#define e int
+#define E main
+#define ee (
+#define eE )
+#define Ee {
+#define eee }
+#define eeE std
+#define Eee return
+#define eEe cout
+#define eeee endl
+
+
+#include <iostream>
+
+e E ee eE
+{
+    eeE::eEe << "lol what" << eeE::eeee;
+    Eee 0;
+}
