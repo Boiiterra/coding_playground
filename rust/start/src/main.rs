12387e7dcbc2024731fb6017f15c6cc1e84d4e82
TerraBoii <kostyaborisenko05@gmail.com>
2022-12-04 10:21:50 +0300

    moved

--- rust/start/src/main.rs
+++ rust/start/src/main.rs
@@ -4,5 +4,5 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
 
-    println!("Your input is {}", input)
+    println!("Your input is:\n{}", input)
 }
