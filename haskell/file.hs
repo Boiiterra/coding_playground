2f9edf382d2ea76ebceac9f13de2b1c2441d5c53
Boiiterra <kostyaborisenko05@gmail.com>
2023-03-07 18:46:14 +0300

    added more haskell

--- haskell/file.hs
+++ haskell/file.hs
@@ -14,6 +14,8 @@ boolInt a = if a then 1 else 0
 isVowel :: Char -> Bool
 isVowel a = a `elem` ['a', 'e', 'i', 'o', 'u']
 
+cool a = init $ tail a
+
 main :: IO()
 main = do
     print $ doubleMe 2
@@ -21,3 +23,13 @@ main = do
     print $ sum $ squareMe [5, 3, 4]
     print $ 1 + boolInt True
     print $ isVowel 'a'
+
+    print $ replicate 5 1
+    print $ [x | x <- [50..100], x `mod` 7 == 0]
+
+    print "Some triangles:"
+    print $ [(a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
+    print "End of some triangles."
+
+    print $ minimum [1,2,1,2,3,4,5,6,4,5,6,5,0,6,5,7,9,1,2,4,9,7,4,6,5]
+
